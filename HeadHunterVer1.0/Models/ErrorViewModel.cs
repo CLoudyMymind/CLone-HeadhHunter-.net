96a205c2e22690847e4dcef473fd5dd83a7b07d7
artemsek <sek39244@gmail.com>
2023-05-26 08:11:01 +0600

    Initial Commit

---
+++ HeadHunterVer1.0/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HeadHunterVer1._0.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
