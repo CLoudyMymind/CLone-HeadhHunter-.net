6ef69765097184bfc95f51fdea469906117d662d
Артем <Sek39244@gmail.com>
2023-05-27 10:17:00 +0600

    Созданы контроллеры для работы с работодателем и соискателем

---
+++ HeadHunterVer1.0/Controllers/EmployeeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HeadHunterVer1._0.Controllers;
+/// <summary>
+/// Вся логика работ с  Соискателем(работником)
+/// </summary>
+public class EmployeeController : Controller
+{
+    
+}
---
+++ HeadHunterVer1.0/Controllers/EmployerController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HeadHunterVer1._0.Controllers;
+/// <summary>
+/// Вся логика действий с работодателем
+/// </summary>
+public class EmployerController : Controller
+{
+    
+}
