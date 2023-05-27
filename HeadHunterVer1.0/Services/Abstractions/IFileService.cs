621a02f42d32271e990953349f39a23d8e3569c7
Артем <Sek39244@gmail.com>
2023-05-27 10:19:26 +0600

    Реализован файл сервис для работы с файлами pdf картинок и других

---
+++ HeadHunterVer1.0/Services/Abstractions/IFileService.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+using HeadHunterVer1._0.ViewModels;
+
+namespace HeadHunterVer1._0.Services.Abstractions;
+// Для работы с файлами, обработка фото и другой логики, связанной с управлением файлами так же
+// ( решил  для формирования pdf file скачать библиотеку
+// iTextSharp офф документация https://github.com/itext/itextsharp
+// формирование файла задумываю реализовать тоже в этом сервисе)
+
+/// <summary>
+/// для работы с сервисом файлов загрузки скичивания файла
+/// </summary>
+public interface IFileService
+{
+    Task<string> FileRegisterCheckAsync(RegisterViewModel model);
+
+}
---
+++ HeadHunterVer1.0/Services/FileService.cs
@@ -0,0 +1,60 @@
+﻿using System.Security.Claims;
+using HeadHunterVer1._0.Services.Abstractions;
+using HeadHunterVer1._0.ViewModels;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace HeadHunterVer1._0.Services;
+
+public class FileService : IFileService
+{
+    private readonly IUserService _userService;
+
+    public FileService(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    public async Task<string> FileRegisterCheckAsync(RegisterViewModel model)
+    {
+        if (model.AvatarFile is not null)
+        {
+            var allowedMimeTypes = new[] { "image/jpeg", "image/jpg", "image/png", "image/webp" };
+            var mimeType = MimeTypes.GetMimeType(Path.GetExtension(model.AvatarFile.FileName));
+            if (!allowedMimeTypes.Contains(mimeType))
+            {
+                throw new Exception();
+            }
+        }
+        var wwwrootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+        var imagesPath = Path.Combine(wwwrootPath, "images");
+        if (!Directory.Exists(imagesPath)) Directory.CreateDirectory(imagesPath);
+
+        string fileName;
+        string filePath;
+        if (model.AvatarFile == null)
+        {
+            fileName = "standart.jpeg";
+            filePath = Path.Combine(wwwrootPath, "ImagesStandart", fileName);
+            var defaultImageBytes = await File.ReadAllBytesAsync(filePath);
+            model.AvatarFile = new FormFile(new MemoryStream(defaultImageBytes), 0, defaultImageBytes.Length,
+                fileName, fileName)
+            {
+                Headers = new HeaderDictionary(),
+                ContentType = "image/jpeg"
+            };
+            var userFilePath = Path.Combine(imagesPath, Guid.NewGuid() + Path.GetExtension(model.AvatarFile.FileName));
+            await using var userStream = new FileStream(userFilePath, FileMode.Create, FileAccess.Write, FileShare.None,
+                4096, FileOptions.Asynchronous);
+            await model.AvatarFile.CopyToAsync(userStream);
+            return Path.GetFileName(userFilePath);
+        }
+
+        fileName = Guid.NewGuid() + Path.GetExtension(model.AvatarFile.FileName);
+        filePath = Path.Combine(imagesPath, fileName);
+        await using var stream = new FileStream(filePath, FileMode.Create);
+        await model.AvatarFile.CopyToAsync(stream);
+
+        return fileName;
+    }
+}
