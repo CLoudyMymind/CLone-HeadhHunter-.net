582791feea6ce8f0bfda9a1df466710f36486e26
artemsek <sek39244@gmail.com>
2023-06-02 11:07:13 +0600

    Обновлена миграция и уалены не нужные файлы

--- HeadHunterVer1.0/Migrations/20230531063022_InitialModelCategoryOrVacancy.Designer.cs
+++ HeadHunterVer1.0/Migrations/20230531063022_InitialModelCategoryOrVacancy.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HeadHunterVer1._0.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace HeadHunterVer1._0.Migrations
 {
     [DbContext(typeof(HeadHunterContext))]
-    partial class HeadHunterContextModelSnapshot : ModelSnapshot
+    [Migration("20230531063022_InitialModelCategoryOrVacancy")]
+    partial class InitialModelCategoryOrVacancy
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HeadHunterVer1.0/Migrations/20230531063022_InitialModelCategoryOrVacancy.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HeadHunterVer1._0.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialModelCategoryOrVacancy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vacancies",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    Wages = table.Column<decimal>(type: "numeric", nullable: false),
+                    IsPublished = table.Column<bool>(type: "boolean", nullable: false),
+                    CategoryId = table.Column<string>(type: "text", nullable: true),
+                    ExperienceYearsFrom = table.Column<int>(type: "integer", nullable: true),
+                    ExperienceYearsTo = table.Column<int>(type: "integer", nullable: true),
+                    UpdateVacancyBid = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vacancies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Vacancies_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Vacancies_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vacancies_CategoryId",
+                table: "Vacancies",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vacancies_UserId",
+                table: "Vacancies",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vacancies");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
