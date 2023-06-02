0add1a36d6f4fde6ce5c907cb13627bf4b7f922a
Артем <Sek39244@gmail.com>
2023-06-02 21:23:06 +0600

    Подключил Бд Удалил не нужный файл

--- HeadHunterVer1.0/Context/HeadHunterContext.cs
+++ HeadHunterVer1.0/Context/HeadHunterContext.cs
@@ -1,4 +1,5 @@
 using HeadHunterVer1._0.Models;
+using HeadHunterVer1._0.Models.Employee;
 using HeadHunterVer1._0.Models.Employer;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +13,10 @@ public class HeadHunterContext : IdentityDbContext<User>
     public DbSet<Course> Courses { get; set; }
     public DbSet<WorkExperience> WorkExperiences { get; set; }
     public DbSet<Resume> Resumes { get; set; }
-    public HeadHunterContext(DbContextOptions<HeadHunterContext> options) : base(options){}
+
+    public HeadHunterContext(DbContextOptions<HeadHunterContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
 
 }
