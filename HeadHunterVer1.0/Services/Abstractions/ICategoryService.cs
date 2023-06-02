49e13e663f1fa9d3be943aad3e0db555aa1f53e7
artemsek <sek39244@gmail.com>
2023-06-02 11:08:13 +0600

    Создан интерфейс сервиса

---
+++ HeadHunterVer1.0/Services/Abstractions/ICategoryService.cs
@@ -0,0 +1,9 @@
+﻿using HeadHunterVer1._0.ViewModels;
+
+namespace HeadHunterVer1._0.Services.Abstractions;
+
+public interface ICategoryService
+{
+    public Task<List<CategoryViewModel>> GetAllCategoryListAsync();
+
+}
