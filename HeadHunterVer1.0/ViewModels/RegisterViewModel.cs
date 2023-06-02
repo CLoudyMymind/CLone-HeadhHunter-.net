5c116cf6a1dd565fa7a2d17ea92a189978a11f07
artemsek <sek39244@gmail.com>
2023-06-02 11:07:50 +0600

    Добавлены атрибуты для валидации

--- HeadHunterVer1.0/ViewModels/RegisterViewModel.cs
+++ HeadHunterVer1.0/ViewModels/RegisterViewModel.cs
@@ -1,22 +1,39 @@
+using System.ComponentModel.DataAnnotations;
+using HeadHunterVer1._0.Attributes;
+
 namespace HeadHunterVer1._0.ViewModels;
-// вьюмодель для регистрации
 public class RegisterViewModel
 {
-    public IFormFile AvatarFile { get; set; }
+    [DataType(DataType.Upload, ErrorMessage = "загрузите фотку")]
+    [Display(Name = "Загрутите Обложку")]
+    [AllowedExtensions(new []{".png" , ".jpeg" , ".jpg" , ".webp" , ".ico" , ".svg"})]
+    public IFormFile? AvatarFile { get; set; }
     
     public string? PathToAvatarFile { get; set; }
-
-
+    
+    [DataType(DataType.Text , ErrorMessage = "тут должен быть только текст")]
+    [Required(ErrorMessage = "Заполните данные")]
+    [Display(Name = "Укажите ваше имя")]
     public string NameCompanyOrUser { get; set; }
     
+    [DataType(DataType.EmailAddress , ErrorMessage = "тут должен быть только email")]
+    [Required(ErrorMessage = "Заполните Email")]
+    [Display(Name = "Укажите Email адрес")]
     public string Email { get; set; }
-
+    
+    [DataType(DataType.Password,ErrorMessage = "Тут должен быть только пароль")]
+    [Compare(nameof(Password), ErrorMessage = "Пароли не совпадают")]
+    [Required(ErrorMessage = "Укажите пароль")]
+    [Display(Name = "Введите пароль")]
     public string ConfirmPassword { get; set; }
     
+    [DataType(DataType.Password,ErrorMessage = "Тут должен быть только пароль")]
+    [Required(ErrorMessage = "Укажите пароль")]
+    [Display(Name = "Введите пароль")]
     public string Password { get; set; }
-    // свойство role под вопросом нужна ли.
-    // задумка по ней создавать и после проверять и добавлять в юзера роль
     
+    [Required(ErrorMessage = "Выберете вашу роль")]
     public string  Role { get; set; }
     
 }
+
