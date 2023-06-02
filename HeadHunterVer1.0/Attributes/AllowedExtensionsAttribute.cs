be6dc391646517112b61df1953b4583f7791d3b8
artemsek <sek39244@gmail.com>
2023-06-02 10:51:18 +0600

    Написан атрибут валидации файла

---
+++ HeadHunterVer1.0/Attributes/AllowedExtensionsAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HeadHunterVer1._0.Attributes;
+
+
+[AttributeUsage(AttributeTargets.Property)]
+public class AllowedExtensionsAttribute : ValidationAttribute
+{
+    private readonly string[] _extensions;
+
+    public AllowedExtensionsAttribute(string[] extensions)
+    {
+        _extensions = extensions;
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is IFormFile file)
+        {
+            var fileExtension = Path.GetExtension(file.FileName.ToLower());
+
+            if (_extensions.All(e => e != fileExtension))
+            {
+                return new ValidationResult($"Недопустимые расширения файлов");
+            }
+            
+        }
+        return ValidationResult.Success;
+    }
+}
