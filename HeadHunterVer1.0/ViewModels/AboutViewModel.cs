a0ae267904fc4bdbdae6ee34b05ae4f27f6bff16
Adilzhan <amateru.sindzzi@gmail.com>
2023-06-04 18:48:35 +0600

    #12 Добавил недостоющие поля в модель

--- HeadHunterVer1.0/ViewModels/AboutViewModel.cs
+++ HeadHunterVer1.0/ViewModels/AboutViewModel.cs
@@ -3,15 +3,12 @@
 public class AboutViewModel
 {
     public string PathFile { get; set; }
-
     public string NameCompanyOrUser { get; set; }
-
     public string Email { get; set; }
-
     public string Role { get; set; }
-    
     public string userId  { get; set; }
-
     public List<VacancyViewModel> VacancyViewModels { get; set; }
-
+    public string NewPassword { get; set; }
+    public string OldPassword { get; set; }
+    public IFormFile AvatarFile { get; set; }
 }
