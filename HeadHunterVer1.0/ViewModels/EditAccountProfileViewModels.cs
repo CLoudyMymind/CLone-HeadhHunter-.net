1a4c915a8612bbd1fa80b1000bb9712ee752d189
Adilzhan <amateru.sindzzi@gmail.com>
2023-06-04 14:47:47 +0600

    -Рефактор

--- HeadHunterVer1.0/ViewModels/EditAccountProfileViewModels.cs
+++ HeadHunterVer1.0/ViewModels/EditAccountProfileViewModels.cs
@@ -5,18 +5,18 @@ namespace HeadHunterVer1._0.ViewModels;
 
 public class EditAccountProfileViewModels
 {
-    [DataType(DataType.Upload, ErrorMessage = "загрузите фотку")]
+    [DataType(DataType.Upload, ErrorMessage = "Загрузите изображение")]
     [Display(Name = "Загрутите Обложку")]
     [AllowedExtensions(new []{".png" , ".jpeg" , ".jpg" , ".webp" , ".ico" , ".svg"})]
     public IFormFile? AvatarFile { get; set; }
     
     public string? PathToAvatarFile { get; set; }
     
-    [DataType(DataType.Text , ErrorMessage = "тут должен быть только текст")]
+    [DataType(DataType.Text , ErrorMessage = "Тут должен быть только текст")]
     [Display(Name = "Укажите ваше имя")]
     public string? NameCompanyOrUser { get; set; }
     
-    [DataType(DataType.EmailAddress , ErrorMessage = "тут должен быть только email")]
+    [DataType(DataType.EmailAddress , ErrorMessage = "Тут должен быть только email")]
     [Display(Name = "Укажите Email адрес")]
     public string? Email { get; set; }
     
