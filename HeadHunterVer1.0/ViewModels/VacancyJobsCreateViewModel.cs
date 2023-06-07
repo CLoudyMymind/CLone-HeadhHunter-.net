89be0930b2201d1d76f491e4a9ad6b6789e549a2
Артем <Sek39244@gmail.com>
2023-06-07 16:18:11 +0600

    Добавил валидацию при создании вакансии

--- HeadHunterVer1.0/ViewModels/VacancyJobsCreateViewModel.cs
+++ HeadHunterVer1.0/ViewModels/VacancyJobsCreateViewModel.cs
@@ -1,4 +1,5 @@
-﻿using HeadHunterVer1._0.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using HeadHunterVer1._0.Models;
 
 namespace HeadHunterVer1._0.ViewModels;
 
@@ -10,15 +11,23 @@ public class VacancyJobsCreateViewModel
 
     public string CategoryId { get; set; }
 
+    [Required(ErrorMessage = "Поле обязательно для заполнения")]
+    [DataType(DataType.Currency , ErrorMessage = "тут должен быть только цифры")]
     public decimal Wages { get; set; }
 
     public bool IsPublished { get; set; }
+    [Required(ErrorMessage = "Поле обязательно для заполнения")]
+    [DataType(DataType.Text , ErrorMessage = "тут должен быть только текст")]
 
     public string Description { get; set; }
     public List<Category> CategoryViewModels { get; set; }
-
+    [Required(ErrorMessage = "Поле обязательно для заполнения")]
+    [Range(0 , 80, ErrorMessage = "Минимальное значение опыта 0 до 80")]
     public int? ExperienceYearsFrom { get; set; }
+    [Range(1 , 80, ErrorMessage = "Минимальное значение опыта 1 до 80")]
+    [Required(ErrorMessage = "Поле обязательно для заполнения")]
     public int? ExperienceYearsTo { get; set; }
-    
+    [Required(ErrorMessage = "Поле обязательно для заполнения")]
+    [DataType(DataType.Text , ErrorMessage = "тут должен быть только текст")]
     public string NameOfVacancy { get; set; }
 }
