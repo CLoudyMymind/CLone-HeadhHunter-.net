73cee17fe409cad43338f61e2e96396fd53e19b9
Adilzhan <amateru.sindzzi@gmail.com>
2023-06-08 19:54:53 +0600

    -Добавил сервис для резюме

---
+++ HeadHunterVer1.0/Services/Abstractions/IResumeService.cs
@@ -0,0 +1,9 @@
+﻿using HeadHunterVer1._0.Models.Employee;
+
+namespace HeadHunterVer1._0.Services.Abstractions;
+
+public interface IResumeService
+{
+    Task<List<Resume>?> GetAllResumesAsync();
+    Task<Resume?> GetResumeById(int id);
+}
---
+++ HeadHunterVer1.0/Services/ResumeService.cs
@@ -0,0 +1,36 @@
+﻿using HeadHunterVer1._0.Context;
+using HeadHunterVer1._0.Models.Employee;
+using HeadHunterVer1._0.Services.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace HeadHunterVer1._0.Services;
+
+public class ResumeService : IResumeService
+{
+    private readonly HeadHunterContext _db;
+
+    public ResumeService(HeadHunterContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<List<Resume>?> GetAllResumesAsync()
+    {
+        return await _db.Resumes
+            .Include(r => r.Employee)
+            .Include(r => r.Category)
+            .Include(r => r.Courses)
+            .Include(r => r.WorkExperiences)
+            .ToListAsync();
+    }
+
+    public async Task<Resume?> GetResumeById(int id)
+    {
+       return await _db.Resumes
+            .Include(r => r.Employee)
+            .Include(r => r.Category)
+            .Include(r => r.Courses)
+            .Include(r => r.WorkExperiences)
+            .FirstOrDefaultAsync(r => r.Id == id);
+    }
+}
