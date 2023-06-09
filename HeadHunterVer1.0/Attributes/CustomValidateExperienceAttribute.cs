fa05db6291632ac2561ff6916b3c07dd342c83a1
Артем <Sek39244@gmail.com>
2023-06-09 20:38:38 +0600

    Создан кастомный атрибут валидации опыта работы для вакансии

---
+++ HeadHunterVer1.0/Attributes/CustomValidateExperienceAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HeadHunterVer1._0.Attributes;
+
+public class CustomValidateExperience : ValidationAttribute
+{
+    private readonly string _fromPropertyName;
+    private readonly string _toPropertyName;
+
+    public CustomValidateExperience(string fromPropertyName, string toPropertyName)
+    {
+        _fromPropertyName = fromPropertyName;
+        _toPropertyName = toPropertyName;
+    }
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        var fromProperty = validationContext.ObjectType.GetProperty(_fromPropertyName);
+        var toProperty = validationContext.ObjectType.GetProperty(_toPropertyName);
+
+        if (fromProperty != null && toProperty != null)
+        {
+            var fromValue = (int?)fromProperty.GetValue(validationContext.ObjectInstance);
+            var toValue = (int?)toProperty.GetValue(validationContext.ObjectInstance);
+
+            if (fromValue.HasValue && toValue.HasValue && fromValue > toValue)
+            {
+                return new ValidationResult("Значение опыта работы 'от' не должно быть больше значения опыта работы 'до'.");
+            }
+        }
+
+        return ValidationResult.Success;
+    }
+}
