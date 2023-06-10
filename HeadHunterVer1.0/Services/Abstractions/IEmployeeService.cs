dce7556f7f9acaf83ed8b0fcb1650fe7817d9e12
Adilzhan <amateru.sindzzi@gmail.com>
2023-06-10 13:12:18 +0600

    -Добавил получение всех резюме

--- HeadHunterVer1.0/Services/Abstractions/IEmployeeService.cs
+++ HeadHunterVer1.0/Services/Abstractions/IEmployeeService.cs
@@ -1,9 +1,12 @@
 ﻿using System.Security.Claims;
+using HeadHunterVer1._0.Models.Employee;
 using HeadHunterVer1._0.ViewModels;
 
 namespace HeadHunterVer1._0.Services.Abstractions;
 
 public interface IEmployeeService
 {
+    Task<List<ResumeViewModel>> GetAllResume();
     Task CreateResumeAsync(CreateResumeViewModel viewModel, ClaimsPrincipal user);
+
 }
--- HeadHunterVer1.0/Services/EmployeeService.cs
+++ HeadHunterVer1.0/Services/EmployeeService.cs
@@ -1,10 +1,12 @@
 ﻿using System.Security.Claims;
 using HeadHunterVer1._0.Context;
+using HeadHunterVer1._0.Extensions;
 using HeadHunterVer1._0.Models;
 using HeadHunterVer1._0.Models.Employee;
 using HeadHunterVer1._0.Services.Abstractions;
 using HeadHunterVer1._0.ViewModels;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace HeadHunterVer1._0.Services;
 
@@ -12,11 +14,23 @@ public class EmployeeService : IEmployeeService
 {
     private readonly HeadHunterContext _db;
     private readonly UserManager<User> _userManager;
+    private readonly MapTo _mapTo;
 
-    public EmployeeService(HeadHunterContext db, UserManager<User> userManager)
+    public EmployeeService(HeadHunterContext db, UserManager<User> userManager, MapTo mapTo)
     {
         _db = db;
         _userManager = userManager;
+        _mapTo = mapTo;
+    }
+
+    public async Task<List<ResumeViewModel>> GetAllResume()
+    {
+        return _mapTo.MapResumeToResumeViewModels(
+            await _db.Resumes
+                .Include(r => r.Courses)
+                .Include(r => r.Employee)
+                .Include(r => r.Category)
+                .Include(r => r.WorkExperiences).ToListAsync());
     }
 
     public async Task CreateResumeAsync(CreateResumeViewModel viewModel, ClaimsPrincipal user)
@@ -42,4 +56,6 @@ public class EmployeeService : IEmployeeService
         await _db.Resumes.AddAsync(newResume);
         await _db.SaveChangesAsync();
     }
+
+  
 }
