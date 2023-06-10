0e8d3a4a91d83118cec96f7d78902bde2a101ec3
Adilzhan <amateru.sindzzi@gmail.com>
2023-06-10 13:11:15 +0600

    -Добавил подробную страницу для резюме

--- HeadHunterVer1.0/Controllers/EmployeeController.cs
+++ HeadHunterVer1.0/Controllers/EmployeeController.cs
@@ -15,18 +15,20 @@ public class EmployeeController : Controller
     private readonly ICategoryService _categoryService;
     private readonly IEmployeeService _employeeService;
     private readonly UserManager<User> _userManager;
+    private readonly IFileService _fileService;
 
     public EmployeeController(
         IAccountService accountService, 
         ICategoryService categoryService, 
-        IEmployeeService employeeService, UserManager<User> userManager)
+        IEmployeeService employeeService, UserManager<User> userManager, IFileService fileService)
     {
         _accountService = accountService;
         _categoryService = categoryService;
         _employeeService = employeeService;
         _userManager = userManager;
+        _fileService = fileService;
     }
-
+    [Authorize(Roles = "employee")]
     [HttpGet]
     public async Task<IActionResult> AboutProfile(string id)
     {
@@ -34,14 +36,14 @@ public class EmployeeController : Controller
             return View(await _accountService.AboutProfileAsync(id , HttpContext.User));
         return NotFound();
     }
-
+    [Authorize(Roles = "employee")]
     [HttpGet]
     [Authorize]
     public async Task<IActionResult> CreateResume()
     {
         return View(new CreateResumeViewModel { CategoryViewModels = await _categoryService.GetAllCategoryViewModelListAsync() });
     }
-    
+    [Authorize(Roles = "employee")]
     [HttpPost]
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> CreateResume(CreateResumeViewModel viewModel)
@@ -49,5 +51,31 @@ public class EmployeeController : Controller
         await _employeeService.CreateResumeAsync(viewModel, User);
         var currentUser = await _userManager.GetUserAsync(User);
         return RedirectToAction("AboutProfile", new {id = currentUser.Id});
+    }
+
+    [HttpGet]
+    [Authorize(Roles = "employer,employee")]
+    public async Task<IActionResult> AboutResume(int id)
+    {
+        var data = await _employeeService.GetAllResume();
+        if (data.Any( r => r.Id == id))
+        {
+            return View(data.FirstOrDefault(r => r.Id == id));
+        }
+
+        return NotFound();
+    }
+    [HttpGet]
+    public async Task<IActionResult> DownloadPdf(int id)
+    {
+        try
+        {
+            return File(await _fileService.GeneratePdfAsync(id, HttpContext.User),
+                _fileService.ContentTypeFile(), _fileService.GeneratePdfFileName(id));
+        }
+        catch (Exception e)
+        {
+            throw new Exception("Произошла ошибка при скачивание файла");
+        }
     }
 }
---
+++ HeadHunterVer1.0/ViewModels/ResumeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using HeadHunterVer1._0.Models;
+using HeadHunterVer1._0.Models.Employee;
+
+namespace HeadHunterVer1._0.ViewModels;
+
+public class ResumeViewModel
+{
+    public int Id { get; set; }
+    public User Employee { get; set; }
+    public string NameOfResume { get; set; }
+    public int ExpectedSalary { get; set; }
+    public string TelegramLink { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string? FacebookLink { get; set; }
+    public string? LinkedInLink { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public Category Category { get; set; }
+    public List<WorkExperience>? WorkExperiences { get; set; }
+    public List<Course>? Courses { get; set; }
+}
