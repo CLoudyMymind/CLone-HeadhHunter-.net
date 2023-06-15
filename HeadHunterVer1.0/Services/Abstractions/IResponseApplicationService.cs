564289e3b9822cdbaa856aba45e54cc88ddd7b80
artemsek <sek39244@gmail.com>
2023-06-15 14:51:00 +0600

    Добавил отклики для соискателей и работодателей

---
+++ HeadHunterVer1.0/Services/Abstractions/IResponseApplicationService.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+using HeadHunterVer1._0.Models;
+using HeadHunterVer1._0.ViewModels;
+
+namespace HeadHunterVer1._0.Services.Abstractions;
+
+public interface IResponseApplicationService
+{
+    public Task Create(VacancyViewModel model);
+
+    Task<ResponseApplication?> GetByIdApplicationViewModelAsync(string? id);
+    Task<List<ApplicationViewModel>> GetAllApplicationViewModelInEmployerAsync(ClaimsPrincipal user);
+    Task<List<ApplicationViewModel>> GetAllApplicationViewModelInEmployeeAsync(ClaimsPrincipal user);
+}
---
+++ HeadHunterVer1.0/ViewModels/ApplicationViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace HeadHunterVer1._0.ViewModels;
+
+public class ApplicationViewModel
+{
+    public string Id { get; set; }
+
+    public bool IsAcceptOrRejectedResponse { get; set; }
+
+
+    public ResumeViewModel ResumeViewModel { get; set; }
+    
+    public VacancyViewModel VacancyViewModel{ get; set; }
+}
