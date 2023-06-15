800a16520fa3b07257d8ec814e8c504b2273156f
artemsek <sek39244@gmail.com>
2023-06-15 15:00:04 +0600

    Попытка реализовать чат

---
+++ HeadHunterVer1.0/Controllers/ChatController.cs
@@ -0,0 +1,27 @@
+﻿using HeadHunterVer1._0.Context;
+using HeadHunterVer1._0.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HeadHunterVer1._0.Controllers;
+
+public class ChatController : Controller
+{
+    private readonly HeadHunterContext _db;
+
+    public ChatController(HeadHunterContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    public IActionResult CreateMessage()
+    {
+        var data = _db.Chats.FirstOrDefault();
+        var dataChat = new MessageViewModel()
+        {
+            MessageText = data.Message
+        };
+        return View(dataChat);
+    }
+}
---
+++ HeadHunterVer1.0/Models/Chat.cs
@@ -0,0 +1,23 @@
+﻿using HeadHunterVer1._0.Models.Employee;
+using HeadHunterVer1._0.Models.Employer;
+
+namespace HeadHunterVer1._0.Models;
+
+public class Chat
+{
+    public string Id { get; set; }
+
+    public DateTime DateSendMessage   { get; set; }
+
+    public User UserEmployee { get; set; }
+
+    public string UserEmployeeId { get; set; }
+
+    public User UserEmployer { get; set; }
+
+    public string UserEmployerId { get; set; }
+
+    public string Message { get; set; }
+    
+
+}
