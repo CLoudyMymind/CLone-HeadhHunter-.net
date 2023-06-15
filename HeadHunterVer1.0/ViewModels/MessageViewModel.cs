e510b72fa8e8a6f5ede9a2dc367775f517c96626
artemsek <sek39244@gmail.com>
2023-06-15 14:56:22 +0600

    вьюмодель для чата

---
+++ HeadHunterVer1.0/ViewModels/MessageViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace HeadHunterVer1._0.ViewModels;
+
+public class MessageViewModel
+{
+    public string MessageText { get; set; }
+
+
+    public string UserEmployeeId { get; set; }
+
+    public string UserEmployerId { get; set; }
+}
